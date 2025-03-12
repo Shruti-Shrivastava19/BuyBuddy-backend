0529d07172c29a286f1f2734ceb6489318005ad2
Shruti Shrivastava <shrivastavashruti2019@gmail.com>
2025-03-12 14:32:46 +0530

    New commit

---
+++ src/test/java/com/Shruti/BuyBuddy/BuyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shruti.BuyBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
